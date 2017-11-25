242faae49d21662b86cce9200d22ecccc40675c3
Laurent Erignoux <lerignoux@gmail.com>
2017-11-26 03:25:00 +0800

    Fixing user fetching with string ID

--- user_handler.py
+++ user_handler.py
@@ -1,5 +1,6 @@
 import logging
 import pymongo
+from bson import ObjectId
 
 from pymongo import MongoClient
 
@@ -17,13 +18,22 @@ class UserHandler(object):
         self.db.user.create_index([('username', pymongo.ASCENDING)], unique=True)
 
     def get_user(self, user_id):
-        return self.user.find_one({'user_id': user_id})
+        return self.user.find_one({'user_id': ObjectId(user_id)})
 
-    def add_user(self, username):
+    def add_user(self, username, password=None):
         entry = {
             "username": username,
         }
+        if password is not None:
+            entry['password'] = password
         return self.user.find_one({'_id': self.user.insert_one(entry).inserted_id})
+
+    def log_user(self, username, password):
+        user = self.user.find_one({'username': username})
+        if user.passord == password:
+            return user
+        else:
+            return None
 
     @classmethod
     def to_dict(cls, user):
